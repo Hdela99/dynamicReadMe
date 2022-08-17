b19d38413efaa692f4cc0f4ec81f1b213791f902
hdela <hfdelacr@ucsd.edu>
2022-08-16 22:04:03 -0700

    Updating markdown, index.js and deleting readME that was premade and putting instructions into exampleREADME until I finish the code to dynamically create a read me

--- index.js
+++ index.js
@@ -1,14 +1,92 @@
+console.log("readme gen running");
+
 // TODO: Include packages needed for this application
-const inq = require('inquirer');
+const inquirer = require('inquirer');
 const fs = require('fs');
+const markDown = require(`./utils/generateMarkdown`)
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "What is your project title?",
+    name: "title"
+  },
+
+  {
+    type: "input",
+    message: "Who created this?",
+    name: "username"
+  },
+
+  {
+    type: "input",
+    message:"Description: ",
+    name: "description"
+  },
+
+  {
+    type: "input",
+    message:"Installation Instructions: ",
+    name: "installation"
+  },
+
+  {
+    type: "input",
+    message:"Usage information: ",
+    name: "usage"
+
+  },
+
+  {
+    type: "list",
+    message: "What licenses are required with this project?",
+    name: "license",
+    choices: ["MIT", "ISC", "GNUPLv3"],
+    filter(val) {
+      return val.toLowerCase();
+    }
+  },
+
+  {
+    type: "input",
+    message: "Who else contributed to this project?",
+    name: "contributions"
+  },
+
+  {
+    type: "input",
+    message: "How do you test the project?",
+    name: "test"
+  },
+  {
+    type: "input",
+    message: "Where can I go with questiosn(email)?",
+    name: "email"
+  },
+
+  {
+  type: "input",
+  message: "Where can I go with questions(github)?",
+  name: "github"  
+  }
+  ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return inquirer.prompt(data)
+    .then((answers) => {
+      const gen = markDown(answers);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  writeToFile("README", questions);
+}
 
 // Function call to initialize app
-init();
+// init();
+writeToFile("readme", questions);
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,56 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  return ` `;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  return ` `;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return ` `;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
 
-`;
+  ## Table of Contents
+  - [Project description](#Description)
+  -[Usage](#Usage)
+  -[Contribution](#Contribution)
+  -[Installation](#Installation)
+  -[Questions](#Questions)
+  -[License](#License)
+
+  ## Description
+  ${data.description}
+
+  ## Usage
+  ${data.usage}
+
+  ## Contributions
+  ${data.contributions}
+
+  ## Installation
+  ${data.installation}
+
+  ## Questions
+  ${data.email}
+  ${data.github}
+
+  ## License
+  ${data.license}
+
+
+  
+  `;
 }
 
 module.exports = generateMarkdown;
